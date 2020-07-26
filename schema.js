09797e58b2fa517fb98ba9f055f33f697ebe9690
Evelyn Rhee <evelynrhee103@gmail.com>
2020-07-26 07:29:17 -0700

    setup babel

--- schema.js
+++ schema.js
@@ -6,6 +6,7 @@ const {
   GraphQLList,
   GraphQLSchema,
 } = require('graphql');
+
 const axios = require('axios');
 
 // Launch Type
@@ -33,21 +34,53 @@ const RocketType = new GraphQLObjectType({
 
 // Root Type that will resolve our endpoint
 const RootType = new GraphQLObjectType({
-  name: 'RootType',
+  name: 'RootQueryType',
   fields: {
     // first we want is launches it's a list of all the launches
     launches: {
       type: new GraphQLList(LaunchType),
-      resolve(parent, args) {
+      resolve(parent, args){
         // get the data, in this case 3rd party API
         return axios
-          .get('https://api.spacexdata.com/v4/launches')
-          .then((res) => res.data);
-      },
+          .get('https://api.spacexdata.com/v3/launches')
+          .then(res => res.data);
+      }
     },
-  },
+    launch: {
+        //single launch
+        type: LaunchType,
+        //the argument to use to get which launch to get
+        args: {
+            flight_number: {
+                type: GraphQLInt
+            }
+        },
+        resolve(parent, args) {
+            return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+            .then( res => res.data);
+        }
+    },
+    rockets: {
+        type: new GraphQLList(RocketType),
+        resolve(parent, args){
+            return axios.get('https://api.spacexdata.com/v3/rockets')
+            .then( res => res.data)
+        }
+    },
+    // broken api
+    rocket: {
+        type: RocketType,
+        args: {
+            id: {type: GraphQLInt}
+        },
+        resolve(parent, args){
+            return axios.get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+            .then( res => res.data)
+        }
+    }
+  }
 });
 
 module.exports = new GraphQLSchema({
-  query: RootType,
+  query: RootType
 });
